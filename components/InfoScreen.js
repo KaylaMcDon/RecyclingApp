a3c8aa238185a7f2ce8ad8d53ba5b018d4509627
koala <koalabeast5556@hotmail.com>
2024-06-12 15:40:05 -0400

    forgot to save

--- components/InfoScreen.js
+++ components/InfoScreen.js
@@ -1,23 +1,13 @@
 import React, { Component } from "react";
 import {View, Text, Button } from "react-native";
 
-import xlsx from "xlsx"
+
 
 export default function InfoScreen() {
   
-
-  const workbook = xlsx.readFile('./CountyMRF-ShedData.xlsx');
-  const sheetName = workbook.SheetNames[0];
-  const sheet = workbook.Sheets[sheetName];
-  const jsonData = xlsx.utils.sheet_to_json(sheet);
-
   return (
     <View>
       <Text>About Screen</Text>
-      <Button
-      title = "Click me"
-      onPress={()=>console.log(jsonData[1].CO_NAME)}
-      />
     </View>
   );
 }
