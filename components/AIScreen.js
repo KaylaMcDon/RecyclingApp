2bfa03c31b316310561e027dca2f1f733cc54636
koala <koalabeast5556@hotmail.com>
2024-06-21 15:12:22 -0400

    pre fs test

--- components/AIScreen.js
+++ components/AIScreen.js
@@ -1,33 +1,51 @@
 import * as tf from '@tensorflow/tfjs';
 import { bundleResourceIO } from '@tensorflow/tfjs-react-native';
+import { useEffect } from 'react';
 
 
 export default function AIScreen() {
     const modelJSON = require("../GraphRecyclingModel/model.json")
     const modelWeights = require("../GraphRecyclingModel/group1-shard1of1.bin")
+    
 
-
-    const loadModel = (async () => {
+    const loadModel = async () => {
 
         await tf.ready()
 
         const model = await tf
         .loadGraphModel(bundleResourceIO(modelJSON, modelWeights))
         .catch((e) => {console.log(e)})
-        console.log("model loaded")
         return(model);
-    })();
+    };
 
-    const practiceImagee = require("../08544447-2712014160.jpg")
-    const recyclingAI = loadModel
 
-    async function predict() {
-        const input = tf.tensor(practiceImagee);
-        const prediction = recyclingAI.executeAsync(input);
-        const predictionArray = prediction.arraySync();
-        console.log(predictionArray)
+    useEffect(() => {
+        const predict = async (model) => {
+            try {
+        
+        
+                const practiceImage = readFile("../08544447-2712014160.jpg")
+                
+                usedImage = tf.browser.fromPixels(practiceImage)
+
+                const prediction = model.predict(usedImage);
+                console.log("prediction", prediction)
+                const predictionArray = prediction.arraySync();
+                console.log("predictionarray", predictionArray)
+                
+            } catch(error) {
+                console.log("error", error);
+            }
+        }
+    
+    const runPrediction = async () => {
+        const model = await loadModel();
+        if (model) {
+            await predict(model)
+        }
     }
 
-    predict();
-}
-    
+
+    runPrediction();
+
+    }, [])}
