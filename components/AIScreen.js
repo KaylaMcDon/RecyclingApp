132811c10914c1362cbc446156732470ad0bd4f0
koala <koalabeast5556@hotmail.com>
2024-06-21 15:22:20 -0400

    AiScreen changes

--- components/AIScreen.js
+++ components/AIScreen.js
@@ -1,6 +1,7 @@
 import * as tf from '@tensorflow/tfjs';
 import { bundleResourceIO } from '@tensorflow/tfjs-react-native';
 import { useEffect } from 'react';
+import { readDir } from 'react-native-fs';
 
 
 export default function AIScreen() {
@@ -24,7 +25,7 @@ export default function AIScreen() {
             try {
         
         
-                const practiceImage = readFile("../08544447-2712014160.jpg")
+                const practiceImage = readDir("../08544447-2712014160.jpg")
                 
                 usedImage = tf.browser.fromPixels(practiceImage)
 
