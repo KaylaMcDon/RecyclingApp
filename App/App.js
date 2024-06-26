8d660d09b67812fe44f9140aee5aa6da0166566a
koala <koalabeast5556@hotmail.com>
2024-06-26 10:31:57 -0400

    fixed navigation to info other files

--- App/App.js
+++ App/App.js
@@ -5,6 +5,8 @@ import { NavigationContainer } from "@react-navigation/native"
 import Location from './components/Location';
 import InfoScreen from './components/InfoScreen';
 import AIScreen from './components/AIScreen'
+import Settings from './components/Settings';
+import { reqDivName } from './components/Location';
 //Icons for tab navigation
 
 
@@ -13,6 +15,7 @@ import AIScreen from './components/AIScreen'
 const Tab = createBottomTabNavigator();
 
 export default function App() {
+    if (reqDivName != null) {
   return (
       <NavigationContainer>
           <Tab.Navigator screenOptions={{
@@ -29,12 +32,36 @@ export default function App() {
                   name="Info"
                   component={InfoScreen}
                 />
-              
                 <Tab.Screen
                   name="AIScreen"
                   component={AIScreen}
                 />
+                <Tab.Screen
+                  name="Settings"
+                  component={Settings}
+                />
           </Tab.Navigator>
       </NavigationContainer>
-  );
+  )
+} else {
+    return (
+        <NavigationContainer>
+            <Tab.Navigator screenOptions={{
+                headerStyle: { elevation: 0, backgroundColor: "#2d61fc", },
+                headerTitleAlign: "center",
+                headerTintColor: "white",
+                cardStyle: { backgroundColor: '#fff' }
+              }}>
+                  <Tab.Screen
+                    name="Select Location"
+                    component={Location}
+                  />
+                  <Tab.Screen
+                    name="Settings"
+                    component={Settings}
+                  />
+            </Tab.Navigator>
+        </NavigationContainer>
+    )
+}
 }
