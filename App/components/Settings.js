e85f35274e087d3f6d713fb738c43ec3c799e0fe
Elias Adali <adali25e@ncssm.edu>
2024-06-27 14:50:55 -0400

    unseparate feedback and settings pages, add delay on multiple issue submissions

--- App/components/Settings.js
+++ App/components/Settings.js
@@ -1,11 +1,18 @@
 import React, { useState } from "react";
-import { View, Text, StyleSheet, ScrollView, TouchableHighlight, Platform, } from "react-native";
+import { View, Text, StyleSheet, ScrollView, TouchableHighlight, Platform, Touchable, TextInput } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import FeedbackForm from "./FeedbackForm.js";
+// import GLOBAL from "./global.js";
 
-export default function Settings() {
-  const questionList = ["How does this app work?", "Where are the recycling symbols/numbers?", "Should I select city or county?"]  
+
+export default function Settings() {  
   const [Questions, setQuestions] = useState("");
+  const [showFeedback, setShowFeedback] = useState(false);
+  const [issueTitle, setIssueTitle] = useState("");
+  const [issueBody, setIssueBody] = useState("");
+  const [issueErrorMsg, setIssueErrorMsg] = useState("");
+  const [lastIssueSend, setLastIssueSend] = useState(Date.now() - 300000)
+
+  const questionList = ["How does this app work?", "Where are the recycling symbols/numbers?", "Should I select city or county?"]
   const answerList = ["Every City and County selects a Material Recovery Facility (abbreviated to MRF) to handle the recycling process of collected items. Each MRF has a list of items they do and don’t recycle, so the recycling laws of a location are determined by its chosen MRF. This app simply uses a list of locations and their MRF’s made by the Department of Environmental Quality.", "North Carolina recycling plants have chosen to identify plastic by shape rather than by the type of plastic. Since the shape of the plastic is tied to the type of plastic used, this means that items labeled with a 1 or 2 are usually recyclable.", "If you live in a city you should select the city option. Occasionally a city may choose to use a different recycling facility than the county it is inside. When this happens, selecting the county you live in may result in incorrect information being displayed."]
 
   const FormatList = ({words}) => {
@@ -63,10 +70,61 @@ export default function Settings() {
         {Questions !== "" ? Questions : <View></View>}
         {Questions !== "" ? <View style={[styles.box, {borderBottomLeftRadius: 8, borderBottomRightRadius: 8, height: 25} ]}></View> : <View></View>}
 
-        <View style={{padding: 16}}>
-          <FeedbackForm/>
-        </View>
-
+        <TouchableHighlight
+          style={[
+            styles.box,
+            {borderTopLeftRadius: 8, borderTopRightRadius: 8, marginTop: 20, },
+            showFeedback ? { padding: 8 } : { borderRadius: 8 }
+          ]}
+          onPress={() => { setShowFeedback(!showFeedback) }}
+        >
+          <Text style={styles.title}>Report a Problem...</Text>
+        </TouchableHighlight>
+        {showFeedback &&
+          <View style={[styles.box, {padding: 16}]}>
+            <TextInput
+              style={[styles.titleBox]}
+              maxLength={100}
+              value={issueTitle}
+              placeholder="Title..."
+              onChangeText={setIssueTitle}
+            />
+            <TextInput
+              style={[styles.bodyBox]}
+              multiline={true}
+              maxLength={600}
+              value={issueBody}
+              placeholder="Please describe the issue. If possible, include steps to reproduce the issue."
+              onChangeText={setIssueBody}
+            />
+            <TouchableHighlight
+              style={styles.goButton}
+              activeOpacity={1}
+              underlayColor={"#fff"}
+              onPress={() => {
+                if (issueTitle !== "" && issueBody !== "") {
+                  if (Date.now() - lastIssueSend > 300000) {
+                    setLastIssueSend(Date.now());
+                    setIssueTitle("");
+                    setIssueBody("");
+                    setShowFeedback(false);
+                    fetch(encodeURI(`http://10.50.17.251/feedback/${issueTitle}/${issueBody}`), {
+                      method: 'POST',
+                      mode: 'no-cors',
+                    });
+                  } else {
+                    setIssueErrorMsg(`Please wait ${Math.floor((lastIssueSend - Date.now() + 300000) / 1000)} seconds before submitting again`);
+                  }
+                } else {
+                  setIssueErrorMsg("Please fill out all fields before submitting issue");
+                }
+              }}
+            >
+              <Text style={styles.goText}>Submit</Text>
+            </TouchableHighlight>
+            <Text>{issueErrorMsg}</Text>
+          </View>
+        }
       </ScrollView>
     </SafeAreaView>
   )
@@ -94,4 +152,27 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 24,
   },
+  titleBox: {
+    height: 40,
+    borderWidth: 1,
+    padding: 10,
+    backgroundColor: "aliceblue",
+  },
+  bodyBox: {
+    height: 200,
+    borderWidth: 1,
+    padding: 10,
+    backgroundColor: "aliceblue",
+  },
+  goButton: {
+    marginTop: 20,
+    padding: 10,
+    backgroundColor: "aliceblue",
+    borderRadius: 8,
+  },
+  goText: {
+    textAlign: "center",
+    fontSize: 16,
+    fontWeight: "bold",
+  }
 });
