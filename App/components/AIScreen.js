beff7fe38d1862156331b612d744d94ccb2dd4b0
Elias Adali <adali25e@ncssm.edu>
2024-06-27 17:01:08 -0400

    clean up AIScreen

--- App/components/AIScreen.js
+++ App/components/AIScreen.js
@@ -8,10 +8,7 @@ import "@tensorflow/tfjs-react-native/dist/platform_react_native"
 import GLOBAL from './global.js'
 import { useIsFocused } from "@react-navigation/native"
 
-
 export default function AIScreen() {
-    
-
     //AI
     var imageTensor = "temp"
     const [isRecyclable, setIsRecyclable] = useState("")
@@ -26,56 +23,37 @@ export default function AIScreen() {
         setAIPrediction("Take a picture to recieve an AI prediction")
     };
 
-    
-
     const loadModel = async () => {
-
-        const model = await tf
-        .loadGraphModel(bundleResourceIO(modelJSON, modelWeights))
+        const model = await tf.loadGraphModel(bundleResourceIO(modelJSON, modelWeights));
         return(model);
     };
-
-    function roundArray(array) {
-        for (let item = 0; item < array.length; item++) {
-            roundedItem = (Math.round(array[item]*1000))/1000
-            array[item]=roundedItem
-        }
-        return(array)
-    }
 
     function predictionFunction() {
         const predict = async (model) => {  
             const prediction = model.predict(imageTensor);
             const predictionArray = prediction.arraySync();
+            const realArray = predictionArray[0];
 
-            roundedArray = roundArray(predictionArray[0])
-
-            const materialArray = ['Aluminium', 'Carton', 'Glass', 'Organic Waste', 'Other Plastics', 'Paper and Cardboard', 'Plastic', 'Textiles', 'Wood']
-            const highestPredictionNum = Math.max(...roundedArray)
-            const numIndex = roundedArray.indexOf(highestPredictionNum)
-            const finalMaterial = materialArray[numIndex]
+            const materialArray = ['Aluminium', 'Carton', 'Glass', 'Organic Waste', 'Other Plastics', 'Paper and Cardboard', 'Plastic', 'Textiles', 'Wood'];
+            const highestPredictionNum = Math.max(...realArray);
+            const numIndex = realArray.indexOf(highestPredictionNum);
+            const finalMaterial = materialArray[numIndex];
             
-            setAIPrediction("The AI detects it as: "+finalMaterial+" with a strength of: "+highestPredictionNum*100+"%.")
+            setAIPrediction("The AI detects it as: "+finalMaterial+" with a strength of: "+Math.round(highestPredictionNum*100)+"%.");
 
-            if (finalMaterial == "Carton" || finalMaterial == "Aluminum" || finalMaterial == "Plastic" || finalMaterial == "Paper and Cardboard" || (GLOBAL.allowGlass && finalMaterial == "Glass")) {
-                setIsRecyclable("This item is recyclable at your location! However please note that the AI is not always accurate, and that results that seem wrong probaly are.")
+            if (finalMaterial == "Carton" || finalMaterial == "Aluminium" || finalMaterial == "Plastic" || finalMaterial == "Paper and Cardboard" || (GLOBAL.allowGlass && finalMaterial == "Glass")) {
+                setIsRecyclable("This item is recyclable at your location! However please note that the AI is not always accurate, and that results that seem wrong probaly are.");
             } else {
-                setIsRecyclable("This item is not recyclable at your location. However please note that the AI is not always accurate, and that this could be incorrect.")
+                setIsRecyclable("This item is not recyclable at your location. However please note that the AI is not always accurate, and that this could be incorrect.");
             }
-
-            
-
         }
-    
-    const runPrediction = async () => {
-        const model = await loadModel();
-        if (model) {
-            await predict(model)
-        }
-    }
 
-    runPrediction();
-
+        (async () => {
+            const model = await loadModel();
+            if (model) {
+                await predict(model);
+            }
+        })();
     }
     
     //Camera to get Image
@@ -85,7 +63,6 @@ export default function AIScreen() {
         // Camera permissions are still loading.
         return (<View />);
     }
-
     if (!permission.granted) {
         // Camera permissions are not granted yet.
         return (
@@ -97,10 +74,9 @@ export default function AIScreen() {
     }
 
     const pictureTaker = async () => {
-        
-        data = await camera.takePictureAsync();
-        setAIPrediction("The AI is processing (this may take a minute)")
-        setIsRecyclable("")
+        const data = await camera.takePictureAsync();
+        setAIPrediction("The AI is processing (this may take a minute)");
+        setIsRecyclable("");
 
         const manipResult = await manipulateAsync(
             data["uri"],
@@ -108,18 +84,16 @@ export default function AIScreen() {
             { format: 'jpeg' }
         );
 
-        
         const response = await fetch(manipResult["uri"]);
         const imageDataArrayBuffer = await response.arrayBuffer();
-        const imageData = new Uint8Array(imageDataArrayBuffer)
-        await tf.ready()
+        const imageData = new Uint8Array(imageDataArrayBuffer);
+        await tf.ready();
 
-        const decodedJpeg = decodeJpeg(imageData)
+        const decodedJpeg = decodeJpeg(imageData);
         const resized = tf.cast(decodedJpeg, 'float32');
-        imageTensor = tf.tensor4d(Array.from(resized.dataSync()),[1,256,256,3])
-        predictionFunction()
+        imageTensor = tf.tensor4d(Array.from(resized.dataSync()),[1,256,256,3]);
+        predictionFunction();
     }
-    
 
     return (
         <View style={styles.container}>
@@ -133,29 +107,28 @@ export default function AIScreen() {
             </CameraView>
         </View>
     );
-
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-      },
-      camera: {
+    },
+    camera: {
         flex: 1,
-      },
-      buttonContainer: {
+    },
+    buttonContainer: {
         flex: 1,
         flexDirection: 'row',
-      },
-      button: {
+    },
+    button: {
         flex: 1,
         alignSelf: 'flex-end',
         alignItems: 'center',
         backgroundColor: 'white',
         width: 10000,
-      },
-      text: {
+    },
+    text: {
         fontSize: 24,
         fontWeight: 'bold',
-      },
+    },
 })
