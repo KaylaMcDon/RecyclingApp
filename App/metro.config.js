881786d2994bf373df951f56772405b7dadab360
koala <koalabeast5556@hotmail.com>
2024-06-28 09:36:19 -0400

    fixed errors when using doctor

--- App/metro.config.js
+++ App/metro.config.js
@@ -1,32 +1,9 @@
-/* module.exports = {
-    resolver: {
-        assetExts: ["bin", "txt", "jpg", "png", "ttf", "json"],
-        sourceExts: ['js', 'jsx', 'ts', 'tsx']
-    }
-}
-*/
+const { getDefaultConfig } = require('expo/metro-config');
 
-const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const config = getDefaultConfig(__dirname);
 
-const defaultConfig = getDefaultConfig(__dirname);
+config.resolver.assetExts.push(
+  'bin'
+);
 
-const {
-  resolver: { sourceExts, assetExts },
-} = getDefaultConfig(__dirname);
-
-const config = {
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: true,
-      },
-    }),
-  },
-  resolver: {
-    sourceExts: sourceExts.filter(ext => ext !== 'json'),
-    assetExts: [...assetExts, 'bin', 'json'],
-  },
-};
-
-module.exports = mergeConfig(defaultConfig, config);
+module.exports = config;
